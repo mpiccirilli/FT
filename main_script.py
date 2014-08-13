38f9fb588f330cc97b8e0f40158624aeb997dce7
mpiccirilli <michael.r.piccirilli@gmail.com>
2014-08-13 16:33:03 -0400

    updated script to pull FT articles.  Also counts the number of pos/neg words

--- main_script.py
+++ main_script.py
@@ -1,14 +1,15 @@
+import os
+import re
 from sys import exit
 
 import pandas as pd
 
-import selenium
 from selenium import webdriver
 
 from lxml import html
 import requests
 
-
+os.chdir("/Users/michaelpiccirilli/Documents/GitHub/FT/")
 
 def open_browser():
     
@@ -37,6 +38,7 @@ def open_browser():
 
 def article_search():
     
+    global company
     company = raw_input("What company would you like to search for on www.FT.com? ")
     
     beg_url = "http://search.ft.com/search?q=%r" % company
@@ -49,18 +51,43 @@ def article_search():
     article_links = tree.xpath("//ol/li/h3/a/@href")
     article_date = tree.xpath("//ol/li/div/div/p/text()")
     article_body = []
-    for i in article_links:
+    
+    for i in range(0,len(article_links)):
         try:
-            browser.get(i)
+            browser.get(article_links[i]) #this is not working --> it passes straight to the exception
             article_body.append(browser.find_element_by_id("storyContent").text)
         except:
-            body[i] = "NULL"
+            article_body.append("NULL")
 
-    data = {'date': article_date, 'title': article_titles, 'link': article_links, 'body': article_body, 'company': company}
-    df = pd.DataFrame(data)
- 
-    decision_time = raw_input("Would you like to run another search?  If not, you will be logged out and the browser will close. (y/n): ")
+    structure = {'date': article_date, 'title': article_titles, 'link': article_links, 
+    'body': article_body, 'company': company, 'pos': "", 'neg': "", 'total': "",
+    'pos_perc': "", 'neg_perc': ""}
     
+    data = pd.DataFrame(structure)
+    
+    for i in range(0, len(data)):
+        temp = data['body'][i]    
+        clean = re.sub(r'\W', " ", temp)
+        words = pd.DataFrame(re.split(r'\s+', clean))
+        tally = pd.DataFrame({"Count": pd.value_counts(words[0])})
+        data['pos'][i] = float(sum(pd.merge(pos_words, tally, left_on="Word", right_index=True)['Count']))
+        data['neg'][i] = float(sum(pd.merge(neg_words, tally, left_on="Word", right_index=True)['Count']))
+        data['total'][i] = float(data['pos'][i]) + float(data['neg'][i])
+        
+        try:
+            data['pos_perc'][i] = float(data['pos'][i])/data['total'][i]
+        except:
+            data['pos_perc'][i] = 0
+        
+        try:    
+            data['neg_perc'][i] = float(data['neg'][i])/data['total'][i]
+        except:
+            data['neg_perc'][i] = 0
+
+
+    data.to_csv("%s.csv" % (company), encoding="utf-8", index=False)
+    
+    decision_time = raw_input("Would you like to run another search?  If not, you will be logged out and the browser will close. (y/n): ")
     if decision_time == "y":
         article_search()
     else:
@@ -68,7 +95,12 @@ def article_search():
         browser.quit()
 
 
+
 def start():
+    global pos_words, neg_words
+    pos_words = pd.read_table("positive-words.txt", skiprows=35, sep="\n", names=["Word"])
+    neg_words = pd.read_table("negative-words.txt", skiprows=35, sep="\n", names=["Word"])
+
     choice = raw_input("Do you need to log into FT.com? (y/n): ")
     if choice == "y":
         open_browser()    
@@ -79,3 +111,4 @@ def start():
 
 
 start()
+
